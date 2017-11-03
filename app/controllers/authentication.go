0aae79cd21e9e417ca6c540404c1603506547160
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2017-11-04 04:07:21 +0900

    Authentication

--- app/controllers/authentication.go
+++ app/controllers/authentication.go
@@ -3,6 +3,7 @@ package controllers
 import (
     "github.com/revel/revel"
     "SemiRevel/app/models"
+    "fmt"
 )
 
 type Authentication struct {
@@ -12,13 +13,16 @@ type Authentication struct {
 func (c Authentication) Login() revel.Result {
 
     id := c.Params.Form.Get("id")
-    //password := c.Params.Form.Get("password")
+    password := c.Params.Form.Get("password")
 
-    user := []models.User{}
+    user := models.User{}
     DB.Where("id = ?", id).First(&user)
 
+    if password == user.Password {
+        fmt.Println("認証成功")
+    }
+
        response := JsonResponse{}
-       // この時点でarticleにはidが振られているのでそのまま返してあげます
        response.Response = user
 
        return c.RenderJSON(response)
