c3e5421e3f91c177e561629b4060b6f8c2919fce
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2017-11-04 22:17:44 +0900

    DB変更

--- app/models/material.go
+++ app/models/material.go
@@ -1,12 +1,13 @@
 package models
 
 type Material struct {
-    Material_id     uint64 `gorm:"primary_key" json:"material_id"`
-    Material_name   string `json:"material_name"`
-    Year            string `json:"year"`
-    Month           uint64 `json:"month"`
-    Day             uint64 `json:"day"`
-    Material_type   uint64 `json:"material_type"`
-    File_path       string `json:"file_path"`
-    comment         string `json:"comment"`
+	Material_id   uint64 `gorm:"primary_key" json:"material_id"`
+	Material_name string `json:"material_name"`
+	User_id       string `json:"user_id"`
+	Year          string `json:"year"`
+	Month         uint64 `json:"month"`
+	Day           uint64 `json:"day"`
+	Material_type uint64 `json:"material_type"`
+	File_path     string `json:"file_path"`
+	comment       string `json:"comment"`
 }
--- app/models/user.go
+++ app/models/user.go
@@ -1,8 +1,8 @@
 package models
 
 type User struct {
-    Id          string `gorm:"primary_key" json:"id"`
-    Name        string `json:"name"`
-    Password    string `json:"password"`
-    Grade       uint64 `json:"grade"`
+	Id       string `gorm:"primary_key" json:"id"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+	Grade    string `json:"grade"`
 }
