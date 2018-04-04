549449267a8b54e176c78a277424cbf66387dc27
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2018-04-04 23:57:19 +0900

    [add] gorm

---
+++ app/controllers/gorm.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"SemiRevel/app/models"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	// gormでMySQL接続
+	// 失敗したらerrに格納される
+	// Openの第二引数は {username}:{password}@/{dbname}?charset=utf8&parseTime=True&loc=Local
+	db, err := gorm.Open("mysql", "root:@/semirevel?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	db.DB()
+	// マイグレード
+	db.AutoMigrate(&models.Material{})
+	db.AutoMigrate(&models.User{})
+
+	// dbをDB(*gorm.DB)として外からも使えるようにしてあげます
+	// (関数の外でvarで宣言してるのでこうすれば他のコントローラーからもDB.hoge()って感じで使える)
+	DB = db
+
+}
