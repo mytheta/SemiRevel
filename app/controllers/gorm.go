2c1d6c26d53d3c7c743662eeaf5fd0f9240367af
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2018-04-11 14:57:04 +0900

    [fix] docker file

--- app/controllers/gorm.go
+++ app/controllers/gorm.go
@@ -14,7 +14,7 @@ func InitDB() {
 	// gormでMySQL接続
 	// 失敗したらerrに格納される
 	// Openの第二引数は {username}:{password}@/{dbname}?charset=utf8&parseTime=True&loc=Local
-	db, err := gorm.Open("mysql", "revel:revel@tcp(127.0.0.0:3306)/semirevel?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "revel:revel@tcp(127.0.0.1:3306)/semirevel?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		fmt.Println(err)
 	}
